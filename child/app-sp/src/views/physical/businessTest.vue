203afab9d8df96e070f215cf3dbb5ad5fc467c54
yexinyu <0027015623@iwhalecloud.com>
2022-04-18 10:17:44 +0800

    修改C

--- child/app-sp/src/views/physical/businessTest.vue
+++ child/app-sp/src/views/physical/businessTest.vue
@@ -2,13 +2,22 @@
     <el-dialog
         title="业务测试"
         :visible.sync="visible_sync"
-        width="700px"
+        width="1000px"
         :destroy-on-close="true"
         :close-on-click-modal="false"
         @close="cancel"
     >
         <div>
-            <el-button type="primary" @click="add">新增测试任务</el-button>
+            <el-button type="primary" @click="add" class="m-bottom10">新增测试任务</el-button>
+            <div class="m-bottom20">将对<span class="m-left10">{{'az'}}</span><span class="num_message m-left10">{{'num'}}</span>台物理机  进行业务测试，请确保选中的物理机无客户业务进行。</div>
+            <div class="m-bottom10">云主机创建（在选中的每台物理机上创建1个1核1G的云主机，GPU物理机需创建1个CPU云主机和1个GPU云主机。若创建成功且正常运行，则该项任务测试通过）</div>
+            <div v-for="item in selectedTasks" :key="item.id">
+                <el-select v-model="task">
+                    <el-option v-for="item in taskList" :key="item.id" :value="item" :label="item.name"></el-option>
+                </el-select>
+                <span>该任务需设置参数，请点击<el-button type="text" @click="setParams">设置参数</el-button></span>
+            </div>
+            <div>云主机回收（测试完成，云主机进行销毁）</div>
         </div>
         <span slot="footer" class="dialog-footer">
             <el-button type="primary" @click="confirm">确 定</el-button>
@@ -26,8 +35,23 @@ import SvgIcon from '../../components/svgIcon/index.vue';
 })
 export default class BusinessTest extends Vue{
     @PropSync('visible')visible_sync!:boolean;
+    private taskList = [
+        {id:'1',name:'任务1'},
+        {id:'2',name:'任务2'},
+        {id:'3',name:'任务3'},
+        {id:'4',name:'任务4'},
+        {id:'5',name:'任务5'},
+    ];
+    private selectedTasks = [];
+    private task:any={}
     created(){
         console.log("created",this.visible_sync)
+    }
+    private add(){
+
+    }
+    private setParams(){
+
     }
     private confirm(){
         this.visible_sync=false
