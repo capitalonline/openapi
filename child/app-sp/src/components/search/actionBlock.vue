742cae8c45f3c93d89760ed78fc6f8bd8b874821
xu.guo <xu.guo@capitalonline.net>
2022-04-18 10:37:24 +0800

    大别山三期

--- child/app-sp/src/components/search/actionBlock.vue
+++ child/app-sp/src/components/search/actionBlock.vue
@@ -1,41 +1,81 @@
 <template>
   <div class="action-box">
-      <div class="search-box">
-        <div v-for="(value, key) in search_option" :key="key" class="search-input" :style="value.width ? {width:`${value.width}px`} : {}">
-          <el-select v-model="search_value[key]" v-if="value.list && !value.type" :placeholder="value.placeholder" clearable>
-            <el-option v-for="(item,id) in value.list" :key="Array.isArray(value.list) ? item.type : id" :label="Array.isArray(value.list) ? item.label : item" :value="Array.isArray(value.list) ? item.type : id"></el-option>
+    <div class="search-box">
+      <div
+        v-for="(value, key) in search_option"
+        :key="key"
+        class="search-input"
+        :style="value.width ? { width: `${value.width}px` } : {}"
+      >
+        <el-select
+          v-model="search_value[key]"
+          v-if="value.list && !value.type"
+          :placeholder="value.placeholder"
+          :multiple="value.multiple"
+          clearable
+        >
+          <el-option
+            v-for="(item, id) in value.list"
+            :key="Array.isArray(value.list) ? item.type : id"
+            :label="Array.isArray(value.list) ? item.label : item"
+            :value="Array.isArray(value.list) ? item.type : id"
+          ></el-option>
+        </el-select>
+        <el-input
+          v-model="search_value[key]"
+          :placeholder="value.placeholder"
+          v-if="(!value.list && !value.type) || value.type === 'composite'"
+          clearable
+          class="composite"
+          :style="value.width ? { width: `${value.width}px` } : {}"
+        >
+          <el-select
+            v-model="search_value[key + 'sub']"
+            slot="prepend"
+            placeholder="请选择"
+            v-if="value.type === 'composite'"
+          >
+            <el-option
+              v-for="item in value.list"
+              :key="item.type"
+              :label="item.label"
+              :value="item.type"
+            ></el-option>
           </el-select>
-          <el-input 
-            v-model="search_value[key]" 
-            :placeholder="value.placeholder" 
-            v-if="(!value.list && !value.type) || value.type==='composite'" clearable class="composite" :style="value.width ? {width:`${value.width}px`} : {}">
-            <el-select v-model="search_value[key+'sub']" slot="prepend" placeholder="请选择" v-if="value.type==='composite'">
-              <el-option v-for="item in value.list" :key="item.type" :label="item.label" :value="item.type"></el-option>
-            </el-select>
-          </el-input>
-          <template v-if="value.type==='datetimerange'|| value.type==='daterange'" >
-            <date-picker ref="datepicker"
-              :time_option="search_option[key]" 
-              @fn-emit="FnGetTime($event, key)">
-            </date-picker>
-          </template>
-        </div>
-        
-        <div class="m-bottom20">
-          <el-button type="primary" @click="FnSearch">查 询</el-button>
-          <el-button type="default" @click="FnClear">清 空</el-button>
-        </div>
+        </el-input>
+        <template
+          v-if="value.type === 'datetimerange' || value.type === 'daterange'"
+        >
+          <date-picker
+            ref="datepicker"
+            :time_option="search_option[key]"
+            @fn-emit="FnGetTime($event, key)"
+          >
+          </date-picker>
+        </template>
       </div>
-      <slot>
-        <el-button type="primary" @click="FnShowCreate" v-if="create_btn" :disabled="disabled">{{ create_btn }}</el-button>
-      </slot>
+
+      <div class="m-bottom20">
+        <el-button type="primary" @click="FnSearch">查 询</el-button>
+        <el-button type="default" @click="FnClear">清 空</el-button>
+      </div>
     </div>
+    <slot>
+      <el-button
+        type="primary"
+        @click="FnShowCreate"
+        v-if="create_btn"
+        :disabled="disabled"
+        >{{ create_btn }}</el-button
+      >
+    </slot>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Emit, Vue, Watch } from 'vue-property-decorator';
-import DatePicker from './DatePicker.vue'
-import moment from 'moment'
+import { Component, Prop, Emit, Vue, Watch } from "vue-property-decorator";
+import DatePicker from "./DatePicker.vue";
+import moment from "moment";
 
 @Component({
   components: {
@@ -50,47 +90,47 @@ export default class ActionBlock extends Vue {
   private search_value = {};
   private time: any = null;
   private date_key: string = "";
-  
+
   private FnGetTime(time, key) {
     this.time = time;
     this.date_key = key;
   }
- 
-  @Emit('fn-search')
+
+  @Emit("fn-search")
   private FnSearch() {
-    this.search_value[this.date_key]=this.time
-    return this.search_value
+    this.search_value[this.date_key] = this.time;
+    return this.search_value;
   }
-  @Watch("search_value",{immediate:true,deep:true})
-  private watch_search_value(newval,oldval){
-      
-  }
-  @Emit('fn-create')
-  private FnShowCreate() {
-  }
-  private FnClear(){
-    for(let i in this.search_option){
-      if(["datetimerange", "daterange"].includes(this.search_option[i].type)) {
+  @Watch("search_value", { immediate: true, deep: true })
+  private watch_search_value(newval, oldval) {}
+  @Emit("fn-create")
+  private FnShowCreate() {}
+  private FnClear() {
+    for (let i in this.search_option) {
+      if (["datetimerange", "daterange"].includes(this.search_option[i].type)) {
         (this.$refs.datepicker as any)[0].FnClear();
       } else {
-        this.search_value={}
+        this.search_value = {};
       }
     }
-    this.FnSearch()
+    this.FnSearch();
   }
   private mounted() {
     let flag = 0;
     for (let key in this.search_option) {
       if (this.search_option[key].default_value) {
-        this.$set(this.search_value, key, this.search_option[key].default_value)
-        flag++
+        this.$set(
+          this.search_value,
+          key,
+          this.search_option[key].default_value
+        );
+        flag++;
       }
     }
     if (flag > 0) {
-      setTimeout(()=>{
-        this.FnSearch()
-      },500)
-      
+      setTimeout(() => {
+        this.FnSearch();
+      }, 500);
     }
   }
 }
@@ -110,7 +150,7 @@ export default class ActionBlock extends Vue {
     width: 200px;
     margin-right: 20px;
     margin-bottom: 20px;
-    .el-range-editor.el-input__inner{
+    .el-range-editor.el-input__inner {
       width: 100% !important;
       padding: 0px 10px !important;
     }
@@ -119,22 +159,20 @@ export default class ActionBlock extends Vue {
     width: 100%;
   }
 }
-
-
 </style>
 <style lang="scss">
-.el-picker-panel__footer{
-  .el-button--text{
+.el-picker-panel__footer {
+  .el-button--text {
     display: none !important;
   }
 }
-button.el-button.el-picker-panel__link-btn.el-button--default.el-button--mini.is-plain{
+button.el-button.el-picker-panel__link-btn.el-button--default.el-button--mini.is-plain {
   color: #fff !important;
   background-color: #455cc6 !important;
   border-color: #455cc6 !important;
 }
 
-.composite{
+.composite {
   .el-select .el-input {
     width: 130px;
   }
@@ -142,5 +180,4 @@ button.el-button.el-picker-panel__link-btn.el-button--default.el-button--mini.is
 .input-with-select .el-input-group__prepend {
   background-color: #fff;
 }
-
 </style>
