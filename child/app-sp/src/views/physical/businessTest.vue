2aa140c08120f3ad6fcfcda8cffc476c7554079a
yexinyu <0027015623@iwhalecloud.com>
2022-04-20 10:55:13 +0800

    修改业务测试

--- child/app-sp/src/views/physical/businessTest.vue
+++ child/app-sp/src/views/physical/businessTest.vue
@@ -10,14 +10,16 @@
         <div>
             <el-button type="primary" @click="add" class="m-bottom10">新增测试任务</el-button>
             <div class="m-bottom20">将对<span class="m-left10">{{'az'}}</span><span class="num_message m-left10">{{'num'}}</span>台物理机  进行业务测试，请确保选中的物理机无客户业务进行。</div>
-            <div class="m-bottom10">云主机创建（在选中的每台物理机上创建1个1核1G的云主机，GPU物理机需创建1个CPU云主机和1个GPU云主机。若创建成功且正常运行，则该项任务测试通过）</div>
-            <div v-for="(obj,key) in selectedTasks" :key="key" class="m-bottom10">
-                <el-select v-model="obj.task_id" placeholder="请选择测试任务列表" @change="changeTask(obj)">
-                    <el-option v-for="item in taskList" :key="item.id" :value="item.id" :label="item.name" ></el-option>
+            <div class="m-bottom10"><circle-icon :num="1"></circle-icon>云主机创建（在选中的每台物理机上创建1个1核1G的云主机，GPU物理机需创建1个CPU云主机和1个GPU云主机。若创建成功且正常运行，则该项任务测试通过）</div>
+            <div v-for="(item,index) in selectedTasks" :key="index" class="m-bottom10">
+                <circle-icon :num="index+2"></circle-icon>
+                <el-select v-model="item.task_id" placeholder="请选择测试任务列表" @change="changeTask($event,index)">
+                    <el-option v-for="inn in taskList" :key="inn.id" :value="inn.id" :label="inn.name" :disabled="item.disList.includes(inn.id)"></el-option>
                 </el-select>
-                <span class="m-left10" v-if="obj.task_id">该任务需设置参数，请点击<el-button type="text" @click="setParams">设置参数</el-button></span>
+                <span class="m-left10" v-if="item.task_id">该任务需设置参数，请点击<el-button type="text" @click="setParams">设置参数</el-button></span>
+                <el-button class="m-left10" type="text" @click="del(index)">删除</el-button>
             </div>
-            <div>云主机回收（测试完成，云主机进行销毁）</div>
+            <div><circle-icon :num="selectedTasks.length+2"></circle-icon>云主机回收（测试完成，云主机进行销毁）</div>
         </div>
         <span slot="footer" class="dialog-footer">
             <el-button type="primary" @click="confirm">确 定</el-button>
@@ -28,39 +30,61 @@
 <script lang="ts">
 import {Vue,Component,Prop,PropSync,Watch} from 'vue-property-decorator';
 import SvgIcon from '../../components/svgIcon/index.vue';
+import CircleIcon from './indexIcon.vue';
 @Component({
     components:{
-        SvgIcon
+        SvgIcon,
+        CircleIcon
     }
 })
 export default class BusinessTest extends Vue{
     @PropSync('visible')visible_sync!:boolean;
-    private taskList = [
+    private taskList:Array<any>= [
         {id:'1',name:'任务1'},
         {id:'2',name:'任务2'},
         {id:'3',name:'任务3'},
         {id:'4',name:'任务4'},
         {id:'5',name:'任务5'},
     ];
-    private selectedTasks = {};
+    private selectedTasks:Array<any> = [];
     private num:number=1;
-    private task:string=''
+    private task:string='';
     created(){
-        console.log("created",this.visible_sync)
+    }
+    private set_disList(){
+        let ids = this.selectedTasks.map(task=>task.task_id)//已经选中的任务id
+        this.selectedTasks.map(item=>{
+            item.disList = ids.filter(inn=>inn!==item.task_id)
+            return item;
+        })
+        console.log("this.selectedTasks",this.selectedTasks)
     }
     private add(){
-        if(Object.keys(this.selectedTasks).length===this.taskList.length){
+        if(this.selectedTasks.length===this.taskList.length){
             return;
         }
-        this.selectedTasks = {...this.selectedTasks,[Math.random()*1000]:{task_id:'',params:{}}}
-        console.log("this.selectedTasks",this.selectedTasks)
-
+        this.selectedTasks.push({task_id:'',params:{},disList:[]}) 
+        this.set_disList()
     }
-    private changeTask(){
+    private changeTask(val,key){
+        this.set_disList()
+        // console.log("changeTask",val)
         
+        // console.log("this.selectedTasks",this.selectedTasks)
+    }
+    private del(ind){
+        console.log("del",ind)
+        this.selectedTasks = this.selectedTasks.map((item,index)=>{
+            console.log("index",ind)
+            if(index===ind){
+                return false
+            }
+            return item
+        })
+        this.set_disList()
     }
     private setParams(){
-
+        
     }
     private confirm(){
         this.visible_sync=false
