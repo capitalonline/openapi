dfd17712dea6100fd486f82c81281ba026e78df5
xu.guo <xu.guo@capitalonline.net>
2022-04-27 13:52:57 +0800

    展开

--- child/app-op/vue.config.js
+++ child/app-op/vue.config.js
@@ -61,11 +61,11 @@ module.exports = {
   configureWebpack: {
     output: {
       // 微应用的包名，这里与主应用中注册的微应用名称一致
-      library: "AppSp",
+      library: "AppOp",
       // 将你的 library 暴露为所有的模块定义下都可运行的方式
       libraryTarget: 'umd',
       // 按需加载相关，设置为 webpackJsonp_VueMicroApp 即可
-      jsonpFunction: `webpackJsonp_AppSp`,
+      jsonpFunction: `webpackJsonp_AppOp`,
     }
   },
   chainWebpack: (config) => {
