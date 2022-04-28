918dabbfdb1e761f9178b6f7a2a67a772f386c6b
yexinyu <0027015623@iwhalecloud.com>
2022-04-28 11:02:32 +0800

    修改同步镜像样式

--- child/app-sp/src/views/mirror/syncMirror.vue
+++ child/app-sp/src/views/mirror/syncMirror.vue
@@ -18,10 +18,16 @@
         </el-card>
         <el-card>
             <div slot="header" class="clearfix">镜像同步位置</div>
-            <el-card v-for="item in [...current_az_list,...available_az_list]" :key="item.region_group_name">
-                <div slot="header" class="clearfix">{{item.region_group_name}}</div>
-                <el-checkbox :disabled="!item.region_group_id " :label="item.region_group_name"></el-checkbox>
-            </el-card>
+            <div class="card">
+                <el-card v-for="(item,key) in az_info" :key="key" class="sync">
+                    <div slot="header" class="clearfix">{{key}}</div>
+                    <el-checkbox v-for="(inn,i) in item" :key="i" :label="i" v-if="inn.label!=='not_available_az_list'" :disabled="inn.label==='current_az_list'">{{inn.az_name}}</el-checkbox>
+                    <el-divider></el-divider>
+                    <div class="m-bottom10">不支持可用区</div>
+                    <div v-for="(inn,i) in item" :key="i" v-if="inn.label==='not_available_az_list'">{{inn.az_name}}</div>
+                </el-card>
+            </div>
+            
         </el-card>
         <span slot="footer" class="dialog-footer">
             <el-button type="primary" @click="confirm">确 定</el-button>
@@ -38,7 +44,7 @@ import Service from '../../https/mirror/list'
 })
 export default class SyncMirror extends Vue{
     @PropSync('visible')visible_sync!:boolean;
-    @Prop('')os_id!:string
+    @Prop({default:''})os_id!:string
     private info:any={
         name:{label:'镜像名称',value:'1'},
         id:{label:'镜像ID',value:'2'},
@@ -46,6 +52,7 @@ export default class SyncMirror extends Vue{
         size:{label:'容量',value:'4'},
         type:{label:'类型',value:'5'},
     }
+    private az_info:any={}
     private current_az_list:any=[
              {
               "az_id":"16e6e380-729d-11ec-b62a-1e00e202ff80",
@@ -57,17 +64,43 @@ export default class SyncMirror extends Vue{
               "az_name":"北京A",
               "region_group_id":"408fd19e-fa78-11e6-bd9a-30b49e091019",
               "region_group_name":"中国大陆"},
+              {"az_id":"a18d6c6a-9619-418a-8526-2878f2653d52",
+              "az_name":"北京A2",
+              "region_group_id":"408fd19e-fa78-11e6-bd9a-30b49e091011",
+              "region_group_name":"中国大陆"},
+              {"az_id":"a18d6c6a-9619-418a-8526-2878f2653d51",
+              "az_name":"北京A1",
+              "region_group_id":"408fd19e-fa78-11e6-bd9a-30b49e091019",
+              "region_group_name":"中国大陆"},
+              {"az_id":"a18d6c6a-9619-418a-8526-2878f2653d53",
+              "az_name":"北京A3",
+              "region_group_id":"408fd19e-fa78-11e6-bd9a-30b49e091019",
+              "region_group_name":"中国大陆"},
              {"az_id":"f9755192-b89d-11eb-b74e-1e00e202ff80",
               "az_name":"圣保罗A",
               "region_group_id":"498cc42c-b89d-11eb-b74e-1e00e202ff80",
               "region_group_name":"南美地区"}
          ]
-    private not_available_az_list=[{"az_id":"a18d6c6a-9619-418a-8526-2878f2653d5f",
+    private not_available_az_list=[{
+                "az_id":"a18d6c6a-9619-418a-8526-2878f2653d5f",
               "az_name":"北京A",
               "region_group_id":"408fd19e-fa78-11e6-bd9a-30b49e091019",
               "region_group_name":"中国大陆"}]
-
+    created(){
+        let temp={
+            'current_az_list':this.current_az_list,
+            'available_az_list':this.available_az_list,
+            'not_available_az_list':this.not_available_az_list
+        }
+        for(let i in temp){
+            temp[i].map(item=>{
+                this.az_info[item.region_group_name] = {...this.az_info[item.region_group_name],[item.az_id]:{...item,label:i}}
+            })
+        }
+        console.log("obj",this.az_info)
+    }
     private async get_pub_sync_detail(){
+        this.az_info={}
         let res:any = await Service.get_pub_sync_detail({
             os_id:this.os_id
         })
@@ -77,9 +110,18 @@ export default class SyncMirror extends Vue{
             this.info.system.value = res.data.os_type;
             this.info.size.value = res.data.size;
             this.info.type.value = res.data.display_name;
-            this.current_az_list = res.data.current_az_list;
-            this.available_az_list = res.data.available_az_list;
-            this.not_available_az_list = res.data.not_available_az_list;
+            const {current_az_list,available_az_list,not_available_az_list}=res.data
+            let temp={
+                'current_az_list':current_az_list,
+                'available_az_list':available_az_list,
+                'not_available_az_list':not_available_az_list
+            }
+            for(let i in temp){
+                temp[i].map(item=>{
+                    this.az_info[item.region_group_name] = {...this.az_info[item.region_group_name],[item.az_id]:{...item,label:i}}
+                })
+            }
+            
         }
     }
     private confirm(){
@@ -94,10 +136,20 @@ export default class SyncMirror extends Vue{
 .el-card{
     margin-bottom: 20px;
 }
+.el-card:last-child{
+    margin-bottom: 0;
+}
 .label{
     width:120px;
 }
 .el-col{
     padding-bottom: 12px;
+}
+.card{
+    display: flex;
+    .sync{
+        width: 220px;
+        margin-right: 10px;
+    }
 }
 </style>
