3c40d569494fab64bd653a8143a37bdfa0f5271c
xinyu.ye <xinyu.ye@capitalonline.net>
2022-06-17 17:08:46 +0800

    修改样式隔离

--- app-main/src/main.ts
+++ app-main/src/main.ts
@@ -44,8 +44,8 @@ async function Init() {
 
   await getUserInfo()
 
-  // startQiankun({sandbox: {experimentalStyleIsolation: true}})
-  startQiankun()
+  startQiankun({sandbox: {strictStyleIsolation: true}})
+  // startQiankun()
   new Vue({
     router,
     store,
