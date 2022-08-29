5b70287a6f3ae575cbeecad7aa29bb8b22688a84
yulong.deng <yulong.deng@capitalonline.net>
2022-08-29 14:37:55 +0800

    add billing example

---
+++ example/Golang/test/bills_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"example/model"
+	"fmt"
+	"testing"
+)
+
+func TestDescribeBill(t *testing.T) {
+	res, _ := model.DescribeBill()
+	fmt.Println(res)
+}
+
+func TestDescribeBillInfo(t *testing.T) {
+	res, _ := model.DescribeBillInfo()
+	fmt.Println(res)
+}
+
+func TestDescribeBillDetail(t *testing.T) {
+	res, _ := model.DescribeBillDetail()
+	fmt.Println(res)
+}
+func TestDescribeAccountInfo(t *testing.T) {
+	res, _ := model.DescribeAccountInfo()
+	fmt.Println(res)
+}
