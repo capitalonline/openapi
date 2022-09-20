51838e4569d2d463ebad3bd03dd8d3f7cb0d0af0
xinyu.ye <xinyu.ye@capitalonline.net>
2022-09-20 10:40:59 +0800

    合并冲突10

--- child/app-sp/src/https/http.ts
+++ child/app-sp/src/https/http.ts
@@ -56,7 +56,6 @@ instance.interceptors.response.use(
     if (loadingCount <= 0) {
       loadingInstance.close();
     }
-    console.log("error",error)
     if(error.response.status == 401) {
     } else {
       
