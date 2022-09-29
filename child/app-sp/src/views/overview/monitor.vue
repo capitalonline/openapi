398d26b2e55c597e032efc213f9e0a911c261e87
xinyu.ye <xinyu.ye@capitalonline.net>
2022-09-29 17:59:43 +0800

    联调监控

--- child/app-sp/src/views/overview/monitor.vue
+++ child/app-sp/src/views/overview/monitor.vue
@@ -53,8 +53,8 @@ export default class OverViewMonitor extends Vue{
     private timeList={
         day: {label: '最近1天', time: 1000 * 60 * 60 * 24},
         week: {label: '最近7天', time: 1000 * 60 * 60* 24 * 7},
-        '30_day': {label: '最近30天', time: 1000 * 60 * 60* 24 * 14},
-        '90_day': {label: '最近90天', time: 1000 * 60 * 60* 24 * 14}
+        '30_day': {label: '最近30天', time: 1000 * 60 * 60* 24 * 30},
+        '90_day': {label: '最近90天', time: 1000 * 60 * 60* 24 * 90}
     }
     private cpu_used = {
         title: 'CPU使用率',
@@ -101,22 +101,21 @@ export default class OverViewMonitor extends Vue{
         this.init()
     }
     private changeProductType(){
-        // this.physicalList=[]
-        this.physicalInfo={}
-        console.log('this.productList',this.productList)
-        let list = this.hostTypes.filter(item=>this.productList.includes(item.host_product_id))
-        console.log('list',list)
-        list.map(item=>{
-            item.host_infos.map(inn=>{
-                this.physicalInfo[inn.host_name]={
-                    hostIp:inn.host_ip,//一个物理机的管理网ip只有一个嘛
-                    region:inn.region_id,
-                    az:inn.az_id,
-                }
+            this.physicalInfo={}
+            let list = this.hostTypes.filter(item=>this.productList.includes(item.host_product_id))
+            list.map(item=>{
+                item.host_infos.map(inn=>{
+                    this.physicalInfo[inn.host_name]={
+                        hostIp:inn.host_ip,//一个物理机的管理网ip只有一个嘛
+                        region:inn.region_id,
+                        az:inn.az_id,
+                    }
+                })
             })
-        })
-        console.log('this.physicalInfo',this.physicalInfo)
-        this.init()
+            this.init()
+        
+        // this.physicalList=[]
+        
     }
     private init(){
         //各个物理机产品之间的物理机会重复嘛
@@ -138,9 +137,58 @@ export default class OverViewMonitor extends Vue{
             Service.get_gpu_percent({...req,queryType:'memory'}),
             Service.get_gpu_percent({...req,queryType:'gpu'}),
         ]).then(res=>{
-
+            console.log('res',res)
+            let obj={
+                0:'cpu_used',
+                1:'memory_used',
+                2:'graphics_ram_used',
+                3:'graphics_card_used'
+            }
+            let showName={
+                sale:'已售量',
+                saleRate:'售卖率',
+                use:'实际使用量',
+                usage:'实际使用率'
+            }
+            res.map((item,index)=>{
+                let list:any=[];
+                let metric:any=[]
+                for(let i in item.data){
+                    if(i!=='xTime'){
+                        metric.push(showName[i])
+                        list.push({
+                            code:item.code,
+                            yValues:[item.data[i]],
+                            xTime:item.data.xTime,
+                        })
+                    }
+                }
+                list.map(inn=>inn.metricInfo = metric)
+                this.FnHandleDubleData(obj[index],list)
+            })
         })
     }
+    private FnHandleDubleData(type,resData) {
+        console.log('resData',resData)
+        let index = 0;
+        this[type].yValue=[]
+        resData.forEach((item: any) => {
+            if (item.code === 'Success') {
+            item.metricInfo = item.metricInfo;
+            if (index === 0) {
+                this[type].xTime = item.xTime;
+                this[type].legend = item.metricInfo;
+            } 
+            if (item.yValues) {
+                this[type].yValue.push(...item.yValues);
+            }
+            }
+            index++;
+        })
+        this[type].resize++;
+        console.log('this.type',this[type])
+  }
+
 }
 </script>
 <style lang="scss" scoped>
