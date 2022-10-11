443ca70c4bfd3f542b4e14d9d73cf0bcd370fcb1
xinyu.ye <xinyu.ye@capitalonline.net>
2022-10-11 15:09:42 +0800

    增加事件列表时间名称搜索

--- child/app-sp/src/views/event/list.vue
+++ child/app-sp/src/views/event/list.vue
@@ -78,6 +78,7 @@ export default class List extends Vue {
   $store;
   private search:any = {
     event_id:  {placeholder:'请输入事件ID'},
+    event_name:  {placeholder:'请输入事件名称'},
     resource_id: {placeholder:'请输入资源ID'},
     task_id: {placeholder:'请输入任务ID'},
     customer_id: {placeholder:'请输入客户ID'},
