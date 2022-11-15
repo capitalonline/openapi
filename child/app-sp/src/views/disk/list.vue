8a2c1ffb74fc74ebfb5fad7c5c9cb8c30a608264
xinyu.ye <xinyu.ye@capitalonline.net>
2022-11-15 18:43:41 +0800

    修改iops云盘删除

--- child/app-sp/src/views/disk/list.vue
+++ child/app-sp/src/views/disk/list.vue
@@ -500,7 +500,7 @@ export default class extends Vue {
     }else if(label==="unInstall"){
       return obj.status==="running" && obj.disk_type==="data" && this.auth_list.includes(label)
     }else if(label==="delete"){
-      return (obj.status==="waiting" || obj.status==="error") && obj.disk_type==="data" && this.auth_list.includes(label)
+      return (obj.status==="waiting" || (obj.status==="error" && obj.is_follow_delete===false)) && obj.disk_type==="data" && this.auth_list.includes(label)
     }else if(label==="restore"){
       return obj.status==="deleted" && obj.is_follow_delete===false && this.auth_list.includes(label)
     }else if(label==="destroy"){
