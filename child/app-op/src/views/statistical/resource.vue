52e850e8af6729f766d93dbfa95f47897545a034
xinyu.ye <xinyu.ye@capitalonline.net>
2023-01-06 14:50:54 +0800

    修改序号

--- child/app-op/src/views/statistical/resource.vue
+++ child/app-op/src/views/statistical/resource.vue
@@ -16,7 +16,7 @@
         >
             <el-table-column prop="customer_id" label="序号" width="45">
                 <template slot-scope="scope">
-                    <span>{{ scope.$index+1 }}</span>
+                    <span>{{ scope.row.index }}</span>
                 </template>
             </el-table-column>
             <el-table-column prop="customer_id" label="客户ID"></el-table-column>
