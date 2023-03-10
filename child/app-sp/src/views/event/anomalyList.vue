7951a54a89f5afe21a1b943166a1611c7fb34c91
xinyu.ye <xinyu.ye@capitalonline.net>
2023-03-10 14:45:37 +0800

    联调任务重试

--- child/app-sp/src/views/event/anomalyList.vue
+++ child/app-sp/src/views/event/anomalyList.vue
@@ -6,48 +6,49 @@
         :data="event_list" 
         border 
         class="event-table"
-        @sort-change="FnSortChange"
         @filter-change="filterAttribute"
       >
-        <el-table-column prop="resource_id" label="资源ID">
-          <template slot-scope="scope">
-            <span class="clickble" @click="view(scope.row.event_id,scope.row.event_name)">{{scope.row.event_id}}</span>
-          </template>
-        </el-table-column>
+        <el-table-column prop="cloud_id" label="资源ID"></el-table-column>
         <el-table-column prop="customer_id" label="客户ID"></el-table-column>
-        <el-table-column prop="customer" label="客户名称"></el-table-column>
-        <el-table-column prop="az_id" label="可用区"></el-table-column>
-        <el-table-column prop="event_id" label="任务ID"></el-table-column>
-        <el-table-column prop="event_name" label="任务名称"></el-table-column>
-        <el-table-column prop="event_state_name" label="任务状态" :filter-multiple="false" column-key="status" :filters="status_list">
+        <el-table-column prop="customer_name" label="客户名称"></el-table-column>
+        <el-table-column prop="az_name" label="可用区"></el-table-column>
+        <el-table-column prop="task_id" label="任务ID"></el-table-column>
+        <el-table-column prop="task_type" label="任务名称"></el-table-column>
+        <el-table-column prop="task_state" label="任务状态"></el-table-column>
+        <el-table-column prop="error_type" label="失败类型" :filter-multiple="false" column-key="error_type" :filters="error_type_list">
           <template slot-scope="scope">
-            <span :class="[scope.row.status === 'failed'|| scope.row.status === 'part_fail' ? 'err' : '']">{{scope.row.event_state_name}}</span>
+            <span>{{scope.row.error_type}}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="implementation_name" label="失败类型"></el-table-column>
-        <el-table-column prop="1" label="当前执行流程"></el-table-column>
-        <el-table-column prop="2" label="失败原因"></el-table-column>
-        <el-table-column prop="create_time" label="创建时间" sortable="custom">
+        <el-table-column prop="subtask_name" label="当前执行流程"></el-table-column>
+        <el-table-column prop="detail" label="失败原因">
+          <template slot-scope="scope">
+            <el-tooltip :content="scope.row.detail" popper-class="tooltip-width" placement="bottom" effect="light">
+              <span>{{ scope.row.detail.length>20 ? `${scope.row.detail.slice(0,21)}...` : scope.row.detail }}</span>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+        <el-table-column prop="create_time" label="创建时间">
           <template slot-scope="scope">
             <span>{{scope.row.create_time ? moment(scope.row.create_time).format("YYYY-MM-DD HH:mm:ss") : '--'}}</span>
           </template>
         </el-table-column>
-        <el-table-column prop="update_time" label="更新时间" sortable="custom">
-          <template slot-scope="scope">
-            <span>{{scope.row.create_time ? moment(scope.row.create_time).format("YYYY-MM-DD HH:mm:ss") : '--'}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column prop="finish_time" label="完成时间" sortable="custom">
+        <el-table-column prop="update_time" label="更新时间">
           <template slot-scope="scope">
             <span>{{scope.row.update_time ? moment(scope.row.update_time).format("YYYY-MM-DD HH:mm:ss") : '--'}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="end_time" label="完成时间">
+          <template slot-scope="scope">
+            <span>{{scope.row.end_time ? moment(scope.row.end_time).format("YYYY-MM-DD HH:mm:ss") : '--'}}</span>
           </template>
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button type="text" 
-              @click="view(scope.row.event_id,scope.row.event_name)"
+              @click="retry(scope.row.task_id,scope.row.event_id)"
             >重试</el-button>
-            <el-button type="text" 
+            <el-button type="text" @click="ignore(scope.row.task_id,scope.row.event_id)"
             >忽略</el-button>
           </template>
         </el-table-column>
@@ -68,6 +69,7 @@
   import { Component, Vue,Watch } from 'vue-property-decorator';
   import ActionBlock from "../../components/search/actionBlock.vue";
   import service from '../../https/event/list';
+  import EcsService from '../../https/instance/create'
   import {trans} from '../../utils/transIndex';
   import moment from 'moment';
   @Component({
@@ -81,9 +83,9 @@
     $store;
     private search:any = {
       az_id:  {placeholder:'请选择可用区',list:[]},
-      resource_id:  {placeholder:'请输入资源ID'},
+      cloud_id:  {placeholder:'请输入资源ID'},
       task_id: {placeholder:'请输入任务ID'},
-      task_name: {placeholder:'请输入任务名称'},
+      task_type: {placeholder:'请输入任务名称'},
       customer_id: {placeholder:'请输入客户ID'},
       customer_name: {placeholder:'请输入客户名称'},
       time:{
@@ -111,84 +113,91 @@
     private event_detail: boolean = false;
     private sort_prop_name:string='';
     private sort_value:string=''
-    private status_list=[];
+    private error_type_list=[];
     private filter_obj={}
     created() {
+      this.get_az_list();
       this.getFilterList()
-      this.event_detail = !!this.$store.state.auth_info["event_detail"]
       this.fn_search()
     }
-     @Watch("$store.state.pod_id")
-      private watch_pod(nv){
-        if(!nv){
-          return;
+    private async get_az_list(){
+      this.search.az_id.list=[]
+    let res:any=await EcsService.get_region_az_list({})
+        if(res.code==="Success"){
+            res.data.forEach(item=>{
+                item.region_list.forEach(inn=>{
+                this.search.az_id.list=[...this.search.az_id.list,...trans(inn.az_list,'az_name','az_id','label','type')]
+                })
+            })
         }
-        this.fn_search(this.search_data)
-      }
+    }
     private async getFilterList(){
-      let res:any=await service.get_filter_list({
-        filed_names:['event_status']
+      let res:any=await service.get_filter_item({
+        filed_names:['error_type']
       })
       if(res.code==='Success'){
-        for(let i in res.data.event_status){
-          this.status_list.push({text:res.data.event_status[i],value:i})
+        for(let i in res.data.error_type){
+          this.error_type_list.push({text:res.data.error_type[i],value:i})
         }
       }
     }
     private fn_search(data:any={}){
       this.current = 1;
       this.search_data = {...data,...this.filter_obj}
-      this.getEventList()
+      this.get_error_task_list()
     }
-    private async getEventList() {
+    private async get_error_task_list() {
       const {search_data:data}=this
-      const res:any = await service.get_event_list({
-        pod_id:this.$store.state.pod_id,
+      const res:any = await service.get_error_task_list({
         page_index:this.current,
         page_size:this.size,
-        event_id:data.event_id || "",
-        event_name:data.event_name || "",
-        resource_id:data.resource_id || "",
-        [this.sort_prop_name]:this.sort_value,
+        az_id:data.az_id || "",
+        cloud_id:data.cloud_id || "",
+        task_type:data.task_type || "",
         task_id:data.task_id || "",
         customer_id:data.customer_id || "",
         customer_name:data.customer_name || "",
-        op_user:data.op_user || "",
-        event_status:data.status ? data.status[0] : undefined,
+        error_type:data.error_type ? data.error_type[0] : undefined,
         start_time:data.time ? moment(data.time[0]).format('YYYY-MM-DD HH:mm:ss') : moment(new Date()).format("YYYY-MM-DD 00:00:00"),
         end_time:data.time ? moment(data.time[1]).format('YYYY-MM-DD HH:mm:ss') : moment(new Date()).format("YYYY-MM-DD HH:mm:ss"),
       })
       if(res.code==="Success"){
-        this.event_list = res.data?.event_list || []
+        this.event_list = res.data?.task_list || []
         this.total = res.data?.page_info?.count || 0
-        this.event_num = res.data?.event_num || 0
-        this.finish_num = res.data?.success_num || 0
-        this.fail_num = res.data?.fail_num || 0
-        this.finished_percent =this.event_num ?  ((this.finish_num / this.event_num)*100).toFixed(2) + '%' : '0.00%'
-        this.error_percent =this.event_num ?   ((this.fail_num / this.event_num)*100).toFixed(2) + '%' : '0.00%'
       }else{
           this.$message.error(res.msg || '操作错误')
         }
     }
     private handleSizeChange(size){
       this.size = size
-      this.getEventList()
+      this.get_error_task_list()
     }
     private handleCurrentChange(cur){
       this.current = cur
-      this.getEventList()
-    }
-    private FnSortChange(obj){
-      this.sort_prop_name = `sort_${obj.prop}`;
-      this.sort_value = obj.order==="descending" ? '0' :obj.order==="ascending" ? '1' : undefined
-      this.getEventList()
+      this.get_error_task_list()
     }
     private filterAttribute(obj:any){
       this.filter_obj = {...this.filter_obj,...obj};
       this.fn_search(this.search_data)
     }
-    private view(id:string,name:string) {
-      this.$router.push({path:`/event/${id}`,query:{name:name ? name : '123'}})
+    private async retry(task_id,event_id) {
+      let res:any = await service.retry_task({
+        task_id,
+        event_id
+      })
+      if(res.code==='Success'){
+        this.$message.success(res.message);
+        this.get_error_task_list()
+      }
+    }
+    private async ignore(task_id) {
+      let res:any = await service.ignore({
+        task_id,
+      })
+      if(res.code==='Success'){
+        this.$message.success(res.message);
+        this.get_error_task_list()
+      }
     }
     
   }
