4c2eb70205bd3949b6328e870c7a88572c39e61c
Administrator <nan.zhu@capitalonline.net>
2023-06-19 10:19:26 +0800

    云盘bug

--- child/app-sp/src/views/snapshot/chains.vue
+++ child/app-sp/src/views/snapshot/chains.vue
@@ -235,7 +235,7 @@ export default class Chains extends Vue {
         //     }})
         // } else {
             console.log('执行', id)
-      this.$router.push({path:'/chain/detail',query:{id:id}}).catch(()=>{})
+      this.$router.push({path:'/chain/detail',query:{id:id}})
             // this.$router.push({name: 'snapshot_detail', query: {
             //   'id': id
             //   }})
