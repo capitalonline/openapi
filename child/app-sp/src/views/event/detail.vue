aedfce3bd380b7f0453c7299835a288e814d94d1
hanlin.zhang <hanlin.zhang@capitalonline.net>
2023-07-10 09:57:37 +0800

    [宕机字迁移]

--- child/app-sp/src/views/event/detail.vue
+++ child/app-sp/src/views/event/detail.vue
@@ -10,7 +10,7 @@
       <!-- <action-block :search_option="search" @fn-search="fn_search"></action-block> -->
       <div class="event-content">
         <div class="event-search">
-          <el-input v-model="cloud_id" placeholder="请输入资源ID或资源名称查询"></el-input>
+          <el-input v-model="cloud_id" placeholder="请输入资源ID或资源名称查询" clearable></el-input>
           <el-button type="primary" @click="getTaskList">搜索</el-button>
         </div>
         <div class="event-detail">
@@ -31,28 +31,76 @@
         @sort-change="FnSortChange"
         @filter-change="filterAttribute"
       >
-        <el-table-column prop="task_id" label="任务ID"></el-table-column>
+        <el-table-column prop="task_id" label="任务ID">
+          <template slot-scope="scope">
+            <span>{{ scope.row.task_id ? scope.row.task_id : '-' }}</span>
+          </template>
+        </el-table-column>
         <!-- <el-table-column prop="event_id" label="事件ID"></el-table-column> -->
-        <el-table-column prop="customer_id" lable="客户ID" :filter-multiple="false"></el-table-column>
-        <el-table-column prop="cloud_id" label="操作资源ID"></el-table-column>
-        <el-table-column prop="az__name" label="可用区"></el-table-column>
+        <el-table-column prop="customer_id" label="客户ID" :filter-multiple="false" column-key="customer_id" :filters="customer_id_list"></el-table-column>
+        <el-table-column prop="customer_name" label="客户名称" :filter-multiple="false" column-key="customer_name" :filters="customer_name_list">
+          <template slot-scope="scope">
+            <span>{{ scope.row.customer_name ? scope.row.customer_name : '-' }}</span>
+          </template>
+        </el-table-column>
+        <!-- <el-table-column prop="cloud_id" label="操作资源ID"></el-table-column> -->
+        <!-- <el-table-column prop="az__name" label="可用区"></el-table-column> -->
+        <el-table-column prop="resource_name" label="资源名称">
+          <template slot-scope="scope">
+            <el-button v-if="scope.row.resource_name != ''" type="text" @click="toPhysical">{{ scope.row.resource_name }}</el-button>
+            <span v-else>-</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="cloud_id" label="资源ID"></el-table-column>
+        <el-table-column prop="" label="资源配置">
+          <template slot-scope="scope">
+            <div v-if="scope.row.resource_name.length > 0" class="resource_name">
+              <el-popover
+                title=""
+                placement="top-start"
+                width="200"
+                trigger="hover"
+                :content="getResourceConfig(scope.row)">
+                <span slot="reference" class="isButton">{{ scope.row.spec_family_name }}</span>
+              </el-popover>
+            </div>
+            <!--   -->
+            <div v-else>
+              -
+            </div>
+          </template>
+        </el-table-column>
         <el-table-column prop="task_type" label="任务名称"></el-table-column>
         <el-table-column prop="task_state" label="任务状态" :filter-multiple="false" column-key="status" :filters="status_list">
           <template slot-scope="scope">
-            <span :class="[scope.row.status === 'failed' ? 'err' : '']">{{scope.row.task_state}}</span>
+            <div class="taskStatus">
+              <div class="tip-icon status-init" v-if="scope.row.status==='init'"></div>
+              <div class="tip-icon status-doing" v-else-if="scope.row.status==='doing'"></div>
+              <div class="tip-icon status-success" v-else-if="scope.row.status==='success'"></div>
+              <div class="tip-icon status-failed" v-else-if="scope.row.status==='failed'"></div>
+              <div class="tip-icon status-failed" v-else></div>
+              <span>{{scope.row.task_state}}</span>
+            </div>
+            
           </template>
+        </el-table-column>
+        <el-table-column prop="detail" label="执行结果">
+            <template slot-scope="scope">
+              <span v-if="scope.row.detail">{{scope.row.detail}}</span>
+              <span v-else>--</span>
+            </template>
         </el-table-column>
          <el-table-column prop="error_type" label="失败类型" :filter-multiple="false" column-key="error_type" :filters="fail_type_list">
           <template slot-scope="scope">
             <span>{{scope.row.error_type ? scope.row.error_type : '-'}}</span>
           </template>
         </el-table-column>
-        <el-table-column label="失败原因">
+        <!-- <el-table-column label="失败原因">
           <template slot-scope="scope">
             <el-button type="text" @click="view(scope.row.detail)" v-if="scope.row.status==='failed'">查看</el-button>
             <span v-else>-</span>
           </template>
-        </el-table-column>
+        </el-table-column> -->
         <el-table-column prop="create_time" label="创建时间" sortable="custom">
           <template slot-scope="scope">
             <span>{{scope.row.create_time ? moment(scope.row.create_time).format("YYYY-MM-DD HH:mm:ss") : '--'}}</span>
@@ -152,9 +200,12 @@ export default class Detail extends Vue {
     private sort_value:string='';
     private filter_obj:any={}
     private cloud_id:string="" //资源id或资源名称查询
+    private customer_id_list=[];
+    private customer_name_list=[];
     created() {
       this.getFilterList()
       this.getTaskList()
+      this.getCurCustomer()
     }
     mounted() {
       this.event_name = this.$route.query.name
@@ -198,16 +249,7 @@ export default class Detail extends Vue {
         error_type:data.error_type ? data.error_type[0] : undefined,
         cloud_id:this.cloud_id ? this.cloud_id : '',
       }
-      console.log('params', params)
-      let res:any = await service.get_task_list({
-        pod_id:this.$store.state.pod_id,
-        status:data.status && data.status.length>0 ? data.status[0] : '',
-        event_id:id,
-        page_index:this.current,
-        page_size:this.size,
-        [this.sort_prop_name]:this.sort_value,
-        error_type:data.error_type ? data.error_type[0] : undefined,
-      })
+      let res:any = await service.get_task_list(params)
       if(res.code === "Success"){
         this.task_list = res.data?.event_list
         this.total = res.data?.page_info?.count || 0
@@ -256,6 +298,37 @@ export default class Detail extends Vue {
     private back(){
       this.$router.go(-1)
     }
+    // 获取当前事件下用户
+    private getCurCustomer() {
+      let params = {
+        event_id: this.$route.params.id,
+      }
+      service.get_customer_list(params).then(res => {
+        if(res.code === "Success") {
+          if(res.data.customer_list.length > 0) {
+            for (let i in res.data.customer_list) {
+              this.customer_id_list.push({text: res.data.customer_list[i].id, value: i})
+              this.customer_name_list.push({text: res.data.customer_list[i].name, value: i})
+            }
+          }
+          
+        }
+      })
+    }
+    private getResourceConfig(row) {
+      let config = ''
+      if(row.gpu_type.length > 0) {
+        config = row.cpu_size + 'C | ' + row.ram_size + 'G | ' + row.gpu_type + ' | ' + row.system_disk_size + 'G ' + row.system_disk_type
+      } else {
+        config = row.cpu_size + 'C | ' + row.ram_size + 'G | -- | ' + row.system_disk_size + 'G ' + row.system_disk_type
+      }
+      return config
+    }
+    private toPhysical() {
+      this.$router.push({
+        path:'/physical',
+      })
+    }
 }
 </script>
 <style lang="scss" scoped>
@@ -290,7 +363,7 @@ export default class Detail extends Vue {
       .event-search {
         margin-bottom: 20px;
         .el-input {
-          width: 200px;
+          width: 220px;
           margin-right: 20px;
         }
       }
@@ -317,7 +390,35 @@ export default class Detail extends Vue {
       margin-top:30px
     }
   }
+  .taskStatus {
+    display: flex;
+    .tip-icon {
+      height: 10px;
+      width: 10px;
+      border-radius: 50%;
+      margin-top: 6px;
+      margin-right: 5px;
+    }
+    .status-init {
+      background-color: #455cc6;
+    }
+    .status-doing {
+      background-color: #e6a23c;
+    }
+    .status-success {
+      background-color: #2DA867;
+    }
+    .status-failed {
+      background-color: #DA3B18;
+    }
+    .status-omit-non {
+      background-color: #666
+    }
+  }
   
+  .isButton {
+    color: #455cc6;
+  }
   
   
 </style>
