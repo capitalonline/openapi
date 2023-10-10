0d2b82b428293dbcec5c39176cedf80e53a943d9
qinyani <yani.qin@capitalonline.net>
2023-10-10 10:51:24 +0800

    test去掉权限 打印菜单列表

--- child/app-op/src/layouts/sidebar.vue
+++ child/app-op/src/layouts/sidebar.vue
@@ -156,6 +156,7 @@ export default class SideBar extends Vue {
           this.menu.push(item)
         }
       }
+      console.log('menu',this.menu)
     })
     // this.routes = this.$router.options.routes.filter(item => !item.meta?.hidden);
     // this.FnWatchRouter(this.$route)
